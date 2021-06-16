5ce70ec30884a2a0c34f5936cd14158f0f74b752
JyotiJanai <31592986+JyotiJanai@users.noreply.github.com>
2021-06-16 12:05:33 -0500

    tiles file updated

---
+++ tile.py
@@ -0,0 +1,7 @@
+# Fill this in with an expression that calculates how many tiles are needed.
+total_tiles=(9*7)+(5*7)
+print(total_tiles)
+
+# Fill this in with an expression that calculates how many tiles will be left over.
+left_tiles=(17*6)-total_tiles
+print(left_tiles)
